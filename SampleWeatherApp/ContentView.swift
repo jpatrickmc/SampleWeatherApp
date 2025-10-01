ad56e9183054935c3d38ea1079505b48961e1c5d
Patrick McDaniel <dfwjpm@outlook.com>
2025-10-01 10:33:54 -0500

    initial commit

---
+++ SampleWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleWeatherApp
+//
+//  Created by Patrick Mcdaniel on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleWeatherApp/SampleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWeatherAppApp.swift
+//  SampleWeatherApp
+//
+//  Created by Patrick Mcdaniel on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
