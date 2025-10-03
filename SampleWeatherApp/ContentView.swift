0845fc8287d2dbdde89af3457f8bac7b7c0e4af1
Patrick McDaniel <dfwjpm@outlook.com>
2025-10-03 15:03:48 -0500

    add LinearGradient

--- SampleWeatherApp/ContentView.swift
+++ SampleWeatherApp/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color(.blue).ignoresSafeArea()
+            
+            LinearGradient(gradient: Gradient(colors: [Color(.red), Color(.blue)]), startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea()
+            
+ 
+            
             Text("London")
                 .font(.system(size: 32, weight: .regular))
                 .foregroundColor(.white)
+             
+                .frame(width: 250, height: 100)
+                .background(Color(.red))
         }
     }
 }
