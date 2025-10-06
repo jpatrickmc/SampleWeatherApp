a236f09af1a10ccf48064d6c4fa00a708b5d15b9
Patrick McDaniel <dfwjpm@outlook.com>
2025-10-06 15:07:20 -0500

    add cloud sun fill image and temp

--- SampleWeatherApp/ContentView.swift
+++ SampleWeatherApp/ContentView.swift
@@ -14,9 +14,24 @@ struct ContentView: View {
             LinearGradient(gradient: Gradient(colors: [Color(.blue), Color(.white)]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .ignoresSafeArea()
             
-            Text("London")
-                .font(.system(size: 32, weight: .regular))
-                .foregroundColor(.white)
+            VStack {
+                Text("London")
+                    .font(.system(size: 32, weight: .regular))
+                    .foregroundColor(.white)
+                
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                
+                Text("20°C")
+                    .font(.system(size: 36))
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+            
         }
     }
 }
